8862693bbd32e4a4299361926715b5eeb395e825
Connor-Mayon <cam2229@nau.edu>
2024-02-23 17:56:39 -0700

    Remove unnecessary lines from server.py

--- server.py
+++ server.py
@@ -20,8 +20,6 @@ def main():
             print('Connected by', addr)
             while True:
                 data = conn.recv(1024)
-                print(data)
-                #if not data: break
                 if data == b'power':
                     if fan_state:
                         fan_state = False
@@ -31,7 +29,6 @@ def main():
                         GPIO.output(pin, GPIO.HIGH)
                 else:
                     GPIO.output(pin, GPIO.LOW)
-
 
 
 if __name__ == "__main__":
