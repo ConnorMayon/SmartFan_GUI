eb4fb19a2ef0cc922be355a5db74adb7ccf03c4b
Connor-Mayon <cam2229@nau.edu>
2024-04-13 14:34:49 -0700

    Move '-' right 1px

--- smartfan/app/gui.py
+++ smartfan/app/gui.py
@@ -92,7 +92,7 @@ class SmartFanApp(App):
         self.min_temp_label = Label(color=[0, 0, 0, 1], text=str(self.min_temp), pos=(65, 300), size_hint=(None, None), size=(70, 60))
         layout.add_widget(self.min_temp_label)
         
-        self.hyphen_label = Label(color=[0, 0, 0, 1], text='-', pos=(112, 300), size_hint=(None, None), size=(70, 60))
+        self.hyphen_label = Label(color=[0, 0, 0, 1], text='-', pos=(113, 300), size_hint=(None, None), size=(70, 60))
         layout.add_widget(self.hyphen_label)
 
         self.max_temp_label = Label(color=[0, 0, 0, 1], text=str(self.max_temp), pos=(170, 300), size_hint=(None, None), size=(70, 60))
